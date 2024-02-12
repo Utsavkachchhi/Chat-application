04693a9bf9792d9e65d3b228ba28cfd2c835abfc
utsav <utsavkachchhi500.com>
2024-02-12 23:36:12 +0530

    react route

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,26 @@ import logo from "./logo.svg";
 import "./App.css";
 import Form from "./modules/Form";
 import Dashboard from "./modules/Dashboard";
+import { Navigate, Route, Routes } from "react-router-dom";
+
+const ProtectedRoute = ({children}) => {
+   const isLoggedIn = localStorage?.getItem('user:token') !== null || true;
+   if(!isLoggedIn) { return <Navigate to='/users/sign_in'/>}
+   else if(isLoggedIn && ['/users/sign_in', 'users/sign_up'].includes(window?.location?.pathname)){
+    return<Navigate to={'/'}/>
+
+   }
+   return children
+}
 
 function App() {
   return (
-    <div className="bg-[#e1edff] h-screen flex justify-center items-center">
-      {/* <Form /> */}
-      <Dashboard/>
-    </div>
+    <Routes>
+      <Route  path="/" element={<ProtectedRoute><Dashboard/></ProtectedRoute>}/>
+       <Route  path="/users/sign_in" element={<ProtectedRoute><Form isSignInPage={true}/></ProtectedRoute>}/>
+       <Route  path="/users/sign_up" element={<ProtectedRoute><Form isSignInPage={false}/></ProtectedRoute>} />
+
+    </Routes>
   );
 }
 
--- client/src/modules/Form/index.js
+++ client/src/modules/Form/index.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Button from "../../components/Button";
 import Input from "../../components/input";
+import { useNavigate } from "react-router-dom";
 
 const Form = ({isSignInPage = true}) => {
 
@@ -12,7 +13,10 @@ const Form = ({isSignInPage = true}) => {
     password : ''
   })
 
+  const navigate = useNavigate();
+
   return (
+    <div className="bg-light h-screen flex items-center justify-center">
     <div className="bg-white w-[600px] h-[800px] shadow-lg rounded-lg flex flex-col justify-center items-center">
       <div className="text-4xl font-extrabold">Welcome {isSignInPage && 'Back'}</div>
      <form className="flex flex-col items-center w-full">
@@ -20,7 +24,7 @@ const Form = ({isSignInPage = true}) => {
         label="Full name"
         name="name"
         placeholder="Enter your full name"
-        className="mb-6"
+        className="mb-6 w-[50%]"
         value={data?.fullName}
         onChange={(e) => setData({...data, fullName:e.target.value})}
       />}
@@ -28,7 +32,7 @@ const Form = ({isSignInPage = true}) => {
         label="Email address"
         name="email"
         placeholder="Enter your email"
-        className="mb-6"
+        className="mb-6 w-[50%]"
         value={data?.email}
         type="email"
         onChange={(e) => setData({...data, email:e.target.value})}
@@ -38,7 +42,7 @@ const Form = ({isSignInPage = true}) => {
         type="password"
         name="password"
         placeholder="Enter your password"
-        className="mb-6"
+        className="mb-14 w-[50%]"
         value={data?.password}
         onChange={(e) => setData({...data, password:e.target.value})}
       />
@@ -46,7 +50,8 @@ const Form = ({isSignInPage = true}) => {
       <Button label={isSignInPage ? 'Sign in' :"Sign up"} type="submit" className="w-1/2 mb-2"/>
 
       </form>
-      <div>{isSignInPage ? "Didn't have an account": "Already have an account"}<span className="text-primary cursor-pointer underline">{isSignInPage ? 'Sign up' : 'Log in'} </span></div>
+      <div >{isSignInPage ? "Didn't have an account": "Already have an account"}<span className="text-primary cursor-pointer underline" onClick={() => navigate(`/users/${isSignInPage ? 'sign_up' : 'sign_in'}`)}>{isSignInPage ? 'Sign up' : 'Log in'} </span></div>
+    </div>
     </div>
   );
 };
